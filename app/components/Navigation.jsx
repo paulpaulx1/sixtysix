70002bf7aa8f75e250ea641245f77ebb4e5b20a0
paulpaulx1 <paulmneenan@gmail.com>
2026-04-12 15:02:18 -0400

    pro logo

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -53,7 +53,7 @@ export default function Navigation() {
             className={styles.logoImage}
             width={180}
             height={60}
-            src="/66-training-logo-orange.png"
+            src="/66_logo-1.png"
             alt="66 Professional Services Logo"
           />
         </Link>
