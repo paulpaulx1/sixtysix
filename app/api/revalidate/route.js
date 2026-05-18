9a9d5fe9afb44f69cbfc9194a59042ddf28763c2
paulpaulx1 <paulmneenan@gmail.com>
2026-05-18 10:11:30 -0400

    something

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,11 +1,19 @@
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 
 export async function POST(request) {
-  const secret = request.nextUrl.searchParams.get("secret");
+  const body = await request.text();
+  const signature = request.headers.get(SIGNATURE_HEADER_NAME);
 
-  if (secret !== process.env.REVALIDATE_SECRET) {
-    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+  const isValid = await isValidSignature(
+    body,
+    signature,
+    process.env.REVALIDATE_SECRET,
+  );
+
+  if (!isValid) {
+    return NextResponse.json({ message: "Invalid signature" }, { status: 401 });
   }
 
   revalidatePath("/", "layout");
